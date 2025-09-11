9310ffd6e251bdd05c5f302bdb683c4a38881fba
maina john <mainajohn5100@gmail.com>
2025-09-11 19:09:48 +0000

    i want this super admin dashboard to have a font family of "inter", sans

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,20 +1,7 @@
-import { Raleway, Montserrat, PT_Sans } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
-export const raleway = Raleway({
+export const inter = Inter({
   subsets: ['latin'],
-  variable: '--font-raleway',
-  display: 'swap',
-});
-
-export const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-  display: 'swap',
-});
-
-export const ptSans = PT_Sans({
-  subsets: ['latin'],
-  variable: '--font-pt-sans',
-  weight: ['400', '700'],
+  variable: '--font-inter',
   display: 'swap',
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,9 +17,9 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['var(--font-raleway)', 'sans-serif'],
-        headline: ['var(--font-montserrat)', 'sans-serif'],
-        sans: ['var(--font-pt-sans)', 'sans-serif'],
+        sans: ['var(--font-inter)', 'sans-serif'],
+        body: ['var(--font-inter)', 'sans-serif'],
+        headline: ['var(--font-inter)', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
